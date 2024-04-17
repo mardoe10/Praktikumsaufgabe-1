dc3c09f62c4d068b756de02a3d756f08318d0362
mardoe10 <mad6649@thi.de>
2024-04-17 16:15:44 +0200

    Aufgabe 1 Studierende ergänzung in Klasse App

--- Lehrveranstaltung/src/App.java
+++ Lehrveranstaltung/src/App.java
@@ -1,7 +1,10 @@
 public class App {
     public static void main(String[] args) throws Exception {
-         // Erzeugen eines Studenten-Objekts
+         
+        // Erzeugen eines Studenten-Objekts
          Studierende student1 = new Studierende("Maria Dörfler", 123456, "Informatik");
 
+        // Methode frageStellen
+        student1.frageStellen("Was kommt alles in der Prüfung dran?");
     }
 }
