bbefd7a7b663122cea350b2e77f0350a20b889e0
Maria <mad6649@thi.de>
2024-04-17 13:44:16 +0200

    Aufgabe 1

--- Lehrveranstaltung/src/Studierende.java
+++ Lehrveranstaltung/src/Studierende.java
@@ -1,3 +1,19 @@
 public class Studierende {
 
+    private String name;
+    private int matrikelnummer;
+    private String studiengang;
+
+    // Konstruktor
+    public Studierende(String name, int matrikelnummer, String studiengang) {
+        this.name = name;
+        this.matrikelnummer = matrikelnummer;
+        this.studiengang = studiengang;
+    }
+
+    // Öffentliche Methode Frageatellen
+    public void frageStellen(String frage) {
+        System.out.println(name + " :"+ frage);
+    }
+
 }
