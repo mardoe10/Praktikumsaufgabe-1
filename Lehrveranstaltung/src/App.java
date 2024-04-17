14201bec9bf097c5e7f5babf9088737f523b0cc9
krboja <krb9461@thi.de>
2024-04-17 15:05:55 +0100

    Aufgabe 1 fertig

--- Lehrveranstaltung/src/App.java
+++ Lehrveranstaltung/src/App.java
@@ -1,5 +1,12 @@
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+ 
+        // Erzeugen eines Dozierenden-Objekts
+        Dozierender dozierender = new Dozierender("Prof. Dr. Müller", "Informatik", "A123");
+
+
+        // Aufruf der Methode "frageBeantworten"
+        dozierender.frageBeantworten("Das, was wir im Praktikum gemacht haben.");
+        
     }
 }
